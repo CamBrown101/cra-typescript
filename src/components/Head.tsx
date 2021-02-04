8877af22f3e45f7a1a966dc26a1c970127b9b834
cambrown101 <cameron-brown@live.com>
2021-02-03 16:56:19 -0800

    Show typescript predictions on required props

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 //Sets types for props. Props word can be anything
+//Title is required and isActive is optional
+//By default params are required
 type Props = {
-  title: string;
-  isActive: boolean;
+  title: string; //Required
+  isActive?: boolean; //Optional
 };
 
-export const Head = ({ title = 'Hello', isActive = true }: Props) => {
+export const Head = ({ title, isActive = true }: Props) => {
   return (
     <div>
       <h1>{title}</h1>
