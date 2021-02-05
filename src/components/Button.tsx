a23d515a569c5a48878281aad02be4936c012b7a
cambrown101 <cameron-brown@live.com>
2021-02-04 17:09:00 -0800

    Typing children props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
+import React, { FC } from 'react';
 
 type Props = {
-  //typing Dom events
-  // onClick: (e: React.MouseEvent) => void;
-  //Button click
-  // onChange?: (e: React.FormEvent<HTMLInputElement><HTMLInputElement>) => void;
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
-export const Button = ({ onClick }: Props) => {
-  return <button onClick={onClick}>Click Me</button>;
+export const Button: React.FC<Props> = ({ onClick, children }) => {
+  return <button onClick={onClick}>{children}</button>;
 };
