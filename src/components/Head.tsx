e095e98907b5c57c71ebb2d30e85c37bc6c63a78
cambrown101 <cameron-brown@live.com>
2021-02-04 16:06:55 -0800

    More on types in React

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -3,9 +3,23 @@ import React from 'react';
 //Sets types for props. Props word can be anything
 //Title is required and isActive is optional
 //By default params are required
+
+type User = {
+  name: string;
+};
+
 type Props = {
   title: string; //Required
   isActive?: boolean; //Optional
+  thing: number;
+  thing2: string[];
+  status: 'loading' | 'loaded';
+  thing3: {};
+  thing4: {
+    name: string;
+  };
+  func: () => void;
+  user: User;
 };
 
 export const Head = ({ title, isActive = true }: Props) => {
