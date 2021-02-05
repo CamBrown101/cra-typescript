192acb4220747d8cf6a977cb6fd01a02ac6f3b76
cambrown101 <cameron-brown@live.com>
2021-02-05 14:34:48 -0800

    adding type to useRef

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 export const Input = () => {
   //Implied types with useState
   const [name, setName] = useState('');
   //Saying it is a string, could also do a "|" and allow it to be more then one type
   // const [name, setName] = useState<string>('');
-  return <input value={name} onChange={(e) => setName(e.target.value)} />;
+  const ref = useRef<HTMLInputElement>(null);
+  if (ref && ref.current) {
+    console.log('ref', ref.current.value);
+  }
+  return (
+    <input ref={ref} value={name} onChange={(e) => setName(e.target.value)} />
+  );
 };
