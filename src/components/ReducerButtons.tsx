3869ea5134b8070417094953d572be128489ca52
cambrown101 <cameron-brown@live.com>
2021-02-05 16:17:21 -0800

    Using useReducer in a more robost way

--- src/components/ReducerButtons.tsx
+++ src/components/ReducerButtons.tsx
@@ -6,9 +6,17 @@ type State = {
   rValue: boolean;
 };
 
-type Action = {
-  type: string;
-};
+//unioning actions
+// type Action = {
+//   type: 'one' | 'two';
+// };
+// if payloads are diffrent between actions
+// type Action = { type: 'one' } | { type: 'two' };
+type Action =
+  | { type: 'one' }
+  | { type: 'two' }
+  | { type: 'three' }
+  | { type: 'four' };
 
 function reducer(state: State, action: Action) {
   switch (action.type) {
@@ -29,6 +37,7 @@ export const ReducerButtons = () => {
       {state && state.rValue && <h1>Visible</h1>}
       <button onClick={() => dispatch({ type: 'one' })}>Action One</button>
       <button onClick={() => dispatch({ type: 'two' })}>Action Two</button>
+      {/* <button onClick={() => dispatch({ type: 'tow' })}>Action Three</button> */}
     </div>
   );
 };
